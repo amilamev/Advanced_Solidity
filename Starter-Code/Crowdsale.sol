ef968b31cbf2e868041921330e0a6c9bbd4a3e68
amilamev <80427410+amilamev@users.noreply.github.com>
2021-09-25 12:11:56 -0500

    update

---
+++ Starter-Code/Crowdsale.sol
@@ -0,0 +1,57 @@
+pragma solidity ^0.5.0;
+
+import "./PupperCoin.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/Crowdsale.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/emission/MintedCrowdsale.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/validation/CappedCrowdsale.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/validation/TimedCrowdsale.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/distribution/RefundablePostDeliveryCrowdsale.sol";
+
+// @TODO: Inherit the crowdsale contracts
+
+contract PupperCoinSale is Crowdsale, MintedCrowdsale, CappedCrowdsale, TimedCrowdsale, RefundablePostDeliveryCrowdsale {
+
+    constructor(
+        string memory name,
+        string memory symbol,
+        address payable wallet,
+        uint rate,
+        uint goal,
+        uint openingTime,
+        uint closingTime,
+        PupperCoin token
+    )
+        Crowdsale(rate, wallet, token)
+        CappedCrowdsale(goal)
+        TimedCrowdsale(openingTime, closingTime)
+        RefundableCrowdsale(goal)
+        public
+    {
+
+    }
+}
+
+contract PupperCoinSaleDeployer {
+
+    address public token_sale_address;
+    address public token_address;
+
+    constructor(
+        string memory name,
+        string memory symbol,
+        address payable wallet, // this address will receive all Ether raised by the sale
+        uint goal
+        
+    )
+        public
+    {
+        PupperCoin token = new PupperCoin(name, symbol, 0);
+        token_address = address(token);
+
+        PupperCoinSale token_sale = new PupperCoinSale(name, symbol, wallet, 1, goal, now, now + 24 weeks, token);
+        token_sale_address = address(token_sale);
+
+        token.addMinter(token_sale_address);
+        token.renounceMinter();
+    }
+} 
